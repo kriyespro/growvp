40e2788f36036c3576197736a2bb8863b52aae0b
Kumar Sunil Verma <kumarsunilverma@gmail.com>
2026-04-27 22:35:17 +0530

    Fix admin static styling for /sd/

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Keep absolute static URL so admin assets resolve correctly under /sd/.
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
